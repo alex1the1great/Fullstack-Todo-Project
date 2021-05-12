02b3ffcec8c335e5c48b1796ae7669fb4996e4e5
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-12 10:41:18 +0545

    [backend]: test todo model

--- todo_backend/todos/tests.py
+++ todo_backend/todos/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Todo
+
+
+class TodoModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Todo.objects.create(title='first todo', description='first todo body')
+
+    def test_title_content(self):
+        todo = Todo.objects.get(id=1)
+        self.assertEqual(todo.title, 'first todo')
+
+    def test_description_content(self):
+        todo = Todo.objects.get(id=1)
+        self.assertEqual(todo.description, 'first todo body')
